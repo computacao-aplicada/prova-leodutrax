29c4c81c38f6e8eb7a799ad8109d72f92859cda3
Leonardo Dutra Da Cruz <leodutrax@Leonardos-MacBook-Air.local>
2025-09-17 21:53:48 -0300

    Commit Final

--- src/Validador.java
+++ src/Validador.java
@@ -1,7 +1,58 @@
 public class Validador {
 
     public static boolean validarCPF(String cpf) {
-        return false;
+        //Verifica se o CPF é nulo ou vazio
+        if (cpf == null || cpf.trim().isEmpty()){
+            System.out.println("Cpf nulo ou vazio! Informe um CPF válido!");
+            return false;
+        }
+
+        //Remove carácteres . ou -
+        String limpo = cpf.trim().replaceAll("[.-]", "");
+
+        //Verifica se o CPF possue apenas números
+        if (!limpo.matches("\\d{11}")) {
+            System.out.println("O CPF informando deve apresentar apenas 11 números. Informe um CPF válido!");
+            return false;
+        }
+
+        //Verifica se o CPF informado possue todos os números iguais
+        if (limpo.chars().distinct().count() == 1) {
+            System.out.println("CPF inválido, todos os números são iguais! Insira um CPF válido!");
+            return false;
+        }
+
+        return checarDigitos(limpo);
+    }
+
+    private static boolean checarDigitos(String cpf) {
+        int[] d = cpf.chars().map(c -> c - '0').toArray();
+
+        // DV1
+        int s1 = 0;
+
+        for (int i = 0; i < 9; i++){
+            s1 += d[i] * (10 - i);
+        }
+
+        int r1 = s1 % 11;
+        int dv1 = (r1 < 2) ? 0 : 11 - r1;
+
+        if (d[9] != dv1){
+            return false;
+        }
+
+        // DV2
+        int s2 = 0;
+
+        for (int i = 0; i < 10; i++){
+            s2 += d[i] * (11 - i);
+        }
+
+        int r2 = s2 % 11;
+        int dv2 = (r2 < 2) ? 0 : 11 - r2;
+
+        return d[10] == dv2;
     }
 
 }
--- tests/ValidadorTest.java
+++ tests/ValidadorTest.java
@@ -1,7 +1,34 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class ValidadorTest {
+    @Test
+    void deveValidarCPFValido() {
+        //CPF com mascára
+        assertTrue(Validador.validarCPF("529.982.247-25"));
+        //CPF sem máscara
+        assertTrue(Validador.validarCPF("52998224725"));
+    }
+
+    @Test
+    void deveRejeitarEntradasInvalidas() {
+        //CPF nulo
+        assertFalse(Validador.validarCPF(null));
+        //CPF vazio
+        assertFalse(Validador.validarCPF(""));
+        //CPF com letra
+        assertFalse(Validador.validarCPF("529.982.247-2X"));
+        //CPF com todos os números iguais
+        assertFalse(Validador.validarCPF("00000000000"));
+    }
+
+    @Test
+    void deveRejeitarTamanhosIncorretos() {
+        //10 dígitos
+        assertFalse(Validador.validarCPF("935.411.347-8"));
+        //12 dígitos
+        assertFalse(Validador.validarCPF("935.411.347-800")); // 12 dígitos
+    }
 }
